c64965aa34419ac654b0115606478bb7f0be9826
DianaYJC <51901766+DianaYJC@users.noreply.github.com>
2020-02-25 16:08:41 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import ReactMapGL, {Marker} from 'react-map-gl';
+
+const token="pk.eyJ1IjoiZGlhbmFkaWZmZXJlbnQiLCJhIjoiY2s3MTc4YnY4MDNtYjNldGh3YTc2NXMyaiJ9.0YUgFH6LXT1s_pXHY4iNKQ"
+function Map() {
+  const [viewport, setViewport] = useState({
+    width: "50vw",
+    height: "50vh",
+    latitude: 37.0902405 ,
+    longitude: -95.7128906,
+    zoom: 3
+  });
+
+  return (
+    <ReactMapGL
+      {...viewport}
+      onViewportChange={setViewport}
+      mapboxApiAccessToken={token} 
+      onViewportChange={(viewport)=>{setViewport(viewport)}}
+      mapStyle="mapbox://styles/dianadifferent/ck72hrnkp1cmd1ilqspv0xx0j"
+    />
+  );
+}
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ 
+  return ( 
+  <div>
+    <h1>Flame</h1>
+    <Map/>
+  </div>
+  )
 }
 
 export default App;
